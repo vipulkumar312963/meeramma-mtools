930e75bd075ce21bfbe8e393b3bdfba8e6114ce7
Vipul Kumar <vipul.kumar312963@gmail.com>
2026-03-19 23:28:57 +0530

    chore: bump version to 1.1-bill-splitter

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "org.meerammafoundation.tools"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1-bill-splitter"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
